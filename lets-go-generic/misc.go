f7dec5e0b55e8297958346c07de0ff67eede8eb5
Daniel Furman <daniel.furman@codilime.com>
2022-02-21 10:57:52 +0100

    Add generic types parameter example

--- lets-go-generic/misc.go
+++ lets-go-generic/misc.go
@@ -18,17 +18,41 @@ func (m MyFloat) String() string {
 // stringable-float# OMIT
 // stringable-float-usage OMIT
 
-func stringifyFloat[T StringableFloat](f T) string { // HL
+func StringifyFloat[T StringableFloat](f T) string { // HL
 	return f.String()
 }
 
 func ExampleMyFloat() {
 	var f MyFloat = 48151623.42
-	s := stringifyFloat[MyFloat](f) // HL
+	s := StringifyFloat[MyFloat](f) // HL
 	fmt.Println(s)                  // 4.815162e+07
 }
 
 // stringable-float-usage# OMIT
-// lesser OMIT
+// generic-constraint OMIT
 
-// lesser# OMIT
+type Slice[E any] interface { // HL
+	~[]E
+}
+
+func FirstElem1[S Slice[E], E any](s S) E { // HL
+	return s[0]
+}
+
+func FirstElem2[S interface{ ~[]E }, E any](s S) E { // HL
+	return s[0]
+}
+
+func FirstElem3[S ~[]E, E any](s S) E { // HL
+	return s[0]
+}
+
+func ExampleSlice() {
+	s := []string{"Go", "rocks"}
+	r1 := FirstElem1(s)
+	r2 := FirstElem2(s)
+	r3 := FirstElem3(s)
+	fmt.Println(r1, r2, r3) // Go Go Go
+}
+
+// generic-constraint# OMIT
